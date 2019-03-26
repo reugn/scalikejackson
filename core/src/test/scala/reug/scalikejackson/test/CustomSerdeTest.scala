c4aa43ea136968442841d72fbcf5319458e23fa6
reugn <reugpro@gmail.com>
2019-03-26 22:11:22 +0200

    test compound object custom writer

---
+++ core/src/test/scala/reug/scalikejackson/test/CustomSerdeTest.scala
@@ -0,0 +1,40 @@
+package reug.scalikejackson.test
+
+import org.scalatest.{FlatSpec, Matchers}
+import reug.scalikejackson.ScalaJacksonImpl._
+import reug.scalikejackson.commons.utils.Resources
+import reug.scalikejackson.play.Json
+import reug.scalikejackson.test.models.MockStruct
+
+class CustomSerdeTest extends FlatSpec with Matchers with Resources {
+
+    "compound object" should "parse properly" in {
+        case class Container(
+                                i_str: String,
+                                i_mock: MockStruct
+                            )
+
+        val mock_instance = MockStruct(1, "a", Some(true))
+        val container_instance = Container("asdf", mock_instance)
+
+        val mock_writes = Json.writes[MockStruct](
+            p => (
+                ("in", classOf[Int], p.i),
+                ("sn", classOf[String], p.s),
+                ("bn", Option(classOf[Boolean]), p.b)
+            )
+        )
+
+        val mock_reads = Json.reads[MockStruct](
+            ("in", classOf[Int]),
+            ("sn", classOf[String]),
+            ("bn", Option(classOf[Boolean]))
+        )
+
+        implicit val mock_format = Json.format[MockStruct] + (mock_writes, mock_reads)
+        implicit val container_format = Json.format[Container] + (mock_writes, mock_reads)
+
+        mock_instance.write shouldBe """{"in":1,"sn":"a","bn":true}"""
+        container_instance.write shouldBe """{"i_str":"asdf","i_mock":{"in":1,"sn":"a","bn":true}}"""
+    }
+}
